a7605b87ad061c98d7a778db65113e97847bf79e
RengoWin <Benjamin.winde@gmail.com>
2025-12-08 14:35:11 +0100

    setup?

---
+++ Assets/Scipts/PlayerStarDataClass.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+[System.Serializable]
+public class PlayerStarDataClass
+{
+    [Header ("Player Base Stats")]
+    [SerializeField] int hitPoints; //Maybe float? or we work with dmg and so only in no float/double values
+    [SerializeField] float movementSpeed;
+
+
+    public int HP
+    {
+        get => hitPoints;
+        set => hitPoints = value;
+    }
+
+    public float MovementSpeed
+    {
+        get => movementSpeed;
+        set => movementSpeed = value;
+    }
+
+}
