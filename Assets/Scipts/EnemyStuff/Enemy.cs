1ce1310c5464deac447543bb43356a456d3cac16
RengoWin <Benjamin.winde@gmail.com>
2025-12-09 15:24:21 +0100

    erste wepoan

---
+++ Assets/Scipts/EnemyStuff/Enemy.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField]
+    float hp, movementSpeed;
+    [SerializeField]
+    int damage;
+    //[SerializeField]
+    //Weapon weapon;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
